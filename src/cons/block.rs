a657fe38d4e5db5fe096f8b33d0060d454c6b00e
arcmarqs <maramadeu@gmail.com>
2022-03-10 19:25:09 +0000

    fixed dependencies, started PoW

---
+++ src/cons/block.rs
@@ -0,0 +1,50 @@
+use chrono::prelude::*;
+use sha2::Sha256;
+use primitive_types::U256;
+use random::prelude::*;
+
+pub struct Block {
+   pub id: u64,
+   pub timestamp: i64,
+   pub nonce: u64,
+   pub prev_hash: U256,
+   pub hash: U256,
+   pub Data: String    
+}
+
+pub struct Chain {
+    pub blocks: Vec<Block>
+}
+
+fn hash(val: T) -> U256 {
+    let mut hasher = Sha256::new();
+    hasher.update(val);
+    let result = hasher.finalize();
+}
+
+impl Chain {
+    fn new() -> Self {
+        Self {block: vec![]}
+    }
+
+    fn start(&mut self) {
+        let mut hasher = Sha256::new();
+        let genesis = Block {
+            id = 0,
+            timestamp = Gmt::now().timestamp(),
+            nonce = rand::random::<u64>(),
+            prev_hash = 0,
+            data = String::from("First block"),
+            hash = hash(data)
+        };
+    }
+
+    fn add_block(&mut self, block: Block) {
+        let last_block = self.blocks.last();
+        if(validate(&block, last_block)){
+            self.block.push(block);
+        } else {
+            print!("invalid block!");
+        }
+    }
+}
