0cf0e04508068ee69e76509af07261d5fdc0bc7b
PatriciaV24 <up201805238@fc.up.pt>
2022-06-01 14:15:24 +0100

    insertion gets and sets

--- src/auctions/auction.rs
+++ src/auctions/auction.rs
@@ -18,3 +18,46 @@ pub struct Auction {
     state: AuctionState,
     auction_id: String,
 }
+impl Auction{
+    pub fn new(title: String, seller: NodeID, initial_price: f32, current_price: f32, highest_price: NodeID, time_remaining: Duration,state: AuctionState,
+        auction_id: String)-> Auction {
+        Auction{
+            title,
+            seller,
+            initial_price,
+            current_price,
+            highest_price,
+            starting_time: DateTime::new(Utc::now()),
+            time_remaining,
+            state,
+            auction_id
+        }
+    }
+
+    pub fn get_title(&mut self) -> String {return title;}
+    pub fn get_seller(&mut self) -> NodeID {return seller;}
+    pub fn get_initial_price(&mut self) -> f32 {return initial_price;}
+    pub fn get_current_price(&mut self) -> f32 {return current_price;}
+    pub fn get_highest_price(&mut self) -> NodeID {return highest_price;}
+    pub fn get_starting_time(&mut self) -> DateTime<Utc> {return starting_time;}
+    pub fn get_time_remaining(&mut self) -> Duration {return time_remaining;}
+    pub fn get_state(&mut self) -> AuctionState {return state;}
+    pub fn get_auction_id(&mut self) -> String {return auction_id;}
+
+    pub fn set_title(&mut self, x: String) { self.title = x;}
+
+    pub fn set_seller(&mut self, x: NodeID) { self.seller = x;}
+
+    pub fn set_initial_price(&mut self, x: f32) { self.initial_price = x;}
+
+    pub fn set_current_price(&mut self, x: f32) { self.current_price = x;}
+    
+    pub fn set_highest_price(&mut self, x: NodeID) { self.highest_price = x;}
+
+    pub fn set_starting_time(&mut self, x: Utc) {
+         self.starting_time.insert(x);
+    }
+    pub fn set_time_remaining(&mut self, x: Duration) { self.time_remaining = x;}
+    pub fn set_state(&mut self, x: AuctionState) { self.state = x;}
+    pub fn set_auction_id(&mut self, x: String) { self.auction_id = x;}
+}
--- src/auctions/peer.rs
+++ src/auctions/peer.rs
@@ -9,3 +9,27 @@ pub struct AuctionPeer {
     subscribed_auctions: HashMap<NodeID, Auction>,
     my_subscribers: HashMap<Auction, Vec<NodeID>>,
 }
+impl AuctionPeer{
+    pub fn new(node: KadNode,id: NodeID, auction: Auction) -> AuctionPeer {
+            AuctionPeer{
+                node,
+                subscribed_auctions: HashMap::new(node, action)
+                my_subscribers: HashMap::new(action, Vec::new(id)),
+        }
+    }
+    fn get_node(&mut self) -> KadNode {return node;}
+    fn get_subscribed_auctions(&mut self) -> HashMap<NodeID, Auction> {return subscribed_auctions;}
+    fn get_my_subscribers(&mut self) -> HashMap<Auction, Vec<NodeID>> {return my_subscribers;}
+    
+    fn set_node(&mut self, x: KadNode) {
+        self.node = x;
+    }
+    
+    fn set_subscribed_auctions(&mut self, x:NodeID, a:Auction>) { 
+        self.subscribed_auctions.insert(x,a);
+    }
+
+    /*fn set_my_subscribers(&mut self, a:Auction, x:Vec<NodeID>>) { 
+        self.my_subscribers.insert(x,a);
+    }*/
+}
