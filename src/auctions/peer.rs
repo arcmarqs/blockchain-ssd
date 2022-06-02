c485bd54a21aefc4238339f79eb18dedeeec6d62
arcmarqs <maramadeu@gmail.com>
2022-06-02 21:51:53 +0100

    Merge branch 'auctions-dev' into kademlia-dev

--- src/auctions/peer.rs
+++ src/auctions/peer.rs
@@ -9,3 +9,24 @@ pub struct AuctionPeer {
     subscribed_auctions: HashMap<NodeID, Auction>,
     my_subscribers: HashMap<Auction, Vec<NodeID>>,
 }
+
+impl AuctionPeer{
+    pub fn new(node: KadNode) -> AuctionPeer {
+            AuctionPeer{
+                node,
+                subscribed_auctions: HashMap::new(),
+                my_subscribers: HashMap::new(),
+        }
+    }
+    fn get_node(&self) -> &KadNode { &self.node }
+    fn get_subscribed_auctions(&self) -> &HashMap<NodeID, Auction> { &self.subscribed_auctions }
+    fn get_my_subscribers(&self) -> &HashMap<Auction, Vec<NodeID>> { &self.my_subscribers }
+
+    fn set_subscribed_auctions(&mut self, x:NodeID, a:Auction) { 
+        self.subscribed_auctions.insert(x,a);
+    }
+
+    /*fn set_my_subscribers(&mut self, a:Auction, x:Vec<NodeID>>) { 
+        //verificar o auction e depois a inserção nodeId
+    }*/
+}
